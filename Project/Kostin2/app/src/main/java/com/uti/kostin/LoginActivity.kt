735f0ca2c23379cd643851051cfba34d10159a22
g.aprindasp20@gmail.com <g.aprindasp20@gmail.com>
2023-06-03 13:21:35 +0700

    master: Upload Project

---
+++ Project/Kostin2/app/src/main/java/com/uti/kostin/LoginActivity.kt
@@ -0,0 +1,104 @@
+package com.uti.kostin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uti.kostin.ui.theme.KostinTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                LoginScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    val (email, setEmail) = remember { mutableStateOf("") }
+    val (password, setPassword) = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(14.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logobr_1remove),
+            // Ganti dengan resource logo Anda
+            contentDescription = "Logo",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(400.dp)
+        )
+        TextField(
+            value = email,
+            onValueChange = setEmail,
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+        TextField(
+            value = password,
+            onValueChange = setPassword,
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 12.dp)
+        )
+
+        Button(
+            onClick = { /* Handle login button click */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp), colors = ButtonDefaults.buttonColors(Color.DarkGray)
+        ) {
+            Text("Log In")
+        }
+        Text(
+            text = "Don't have an account?",
+            modifier = Modifier.padding(top = 16.dp)
+        )
+
+        Button(
+            onClick = { /* Handle sign up button click */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp), colors = ButtonDefaults.buttonColors(Color.DarkGray)
+        ) {
+            Text("Sign Up")
+        }
+    }
+}
